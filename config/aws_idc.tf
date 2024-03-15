5e87aa7d40a4eba73d84fd9fedd9bd9aef9c02f8
Shwetha Umashanker <32020525+shwethaumashanker@users.noreply.github.com>
2024-03-15 12:37:54 -0400

    Create aws_idc.tf

---
+++ config/aws_idc.tf
@@ -0,0 +1,21 @@
+resource "commonfate_access_workflow" "aws" {
+  name                     = "aws"
+  access_duration_seconds  = 60 * 60 * 2
+  try_extend_after_seconds = 60 * 60
+  priority                 = 1
+}
+
+resource "commonfate_aws_account_selector" "select_all" {
+  id                  = "select_all_aws"
+  name                = "Select All AWS"
+  aws_organization_id = "<Your AWS Organization ID>"
+  when                = "true"
+}
+
+resource "commonfate_aws_idc_account_availabilities" "aws" {
+  workflow_id             = commonfate_access_workflow.aws.id
+  aws_permission_set_arn  = <The permission set ARN to grant access to>
+  aws_account_selector_id = commonfate_aws_account_selector.select_all.id
+  aws_identity_store_id   = "<Your Identity Store ID>"
+
+}
