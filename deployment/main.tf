dac2b7b440624db312a21ec6efd16a28c76d7625
Shwetha Umashanker <32020525+shwethaumashanker@users.noreply.github.com>
2024-03-28 12:47:19 -0400

    Updated outputs in deployment/main.tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,6 +1,6 @@
 module "common-fate-deployment" {
   source  = "common-fate/common-fate-deployment/aws"
-  version = "1.22.0"
+  version = "1.28.2"
 
   aws_region = "us-east-1 <replace this>"
 
@@ -8,4 +8,16 @@ module "common-fate-deployment" {
   app_certificate_arn = "<ARN of the certificate>"
 
   app_url = "https://commonfate.example.com <replace this>"
+}
+
+output "outputs" {
+  description = "outputs"
+  value       = module.common-fate-deployment.outputs
+}
+
+output "terraform_client_secret" {
+  description = "terraform client secret"
+  value       = module.common-fate-deployment.terraform_client_secret
+
+  sensitive = true
 }
